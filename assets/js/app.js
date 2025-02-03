3fca92bb8e304a0137864495b7db8ba31f5e94a0
AnzhelikaBryuhova <angelka.bryuhova@yandex.ru>
2025-02-04 00:00:56 +0600

    mogo

---
+++ assets/js/app.js
@@ -0,0 +1,96 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+    /* Fixed Header */
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 500);
+    });
+
+
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+    /* Slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
+
+$(function () {
+  $('#callback-button').click(function (e) {
+    e.preventDefault();
+    $('.modal').addClass('modal_active');
+    $('body').addClass('hidden');
+  });
+
+  $('.modal__close-button').click(function (e) {
+    e.preventDefault();
+    $('.modal').removeClass('modal_active');
+    $('body').removeClass('hidden');
+  });
+
+  $('.modal').mouseup(function (e) {
+    let modalContent = $(".modal__content");
+    if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+      $(this).removeClass('modal_active');
+      $('body').removeClass('hidden');
+    }
+  });
+});
